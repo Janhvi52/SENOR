4e704a928a4e1412c0ff0366eab98851fed03baa
Janhvi Singh <janhvi52@gmail.com>
2024-04-12 22:59:46 +0530

    updated backend link

--- senor-try/src/components/Button.jsx
+++ senor-try/src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = () => {
   return (
     <div className='button_link'>
         {/* <a href={CV} download>Resume</a> */}
-        <a href="https://enchatty-5tgbqv7ecfnupiwxgeteey.streamlit.app/" className='btn'>Let's have a Walkthrough</a>
+        <a href="https://senor-legal-advisor.streamlit.app/" className='btn'>Let's have a Walkthrough</a>
     </div>
   )
 }
